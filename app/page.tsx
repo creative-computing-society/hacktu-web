14d53da4bc74cb4feb24502ab042d9b486310a47
Aargh Rai <shubh2007rai@gmail.com>
2026-01-11 10:26:07 +0530

    timeline fix

--- app/page.tsx
+++ app/page.tsx
@@ -132,11 +132,11 @@ function Tracks() {
 
 function Timeline() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const grouped = [] as TimelineItem[][];
+  const grouped = [] as { item: TimelineItem, index: number }[][];
   for (let i = 0; i < TIMELINE.length; i++) {
     if (i % 2 == 0) grouped.push([]);
     console.assert(grouped.length > 0);
-    grouped[grouped.length - 1].push(TIMELINE[i]);
+    grouped[grouped.length - 1].push({ item: TIMELINE[i], index: i });
   }
   return (
     <div className="flex flex-col items-center gap-4">
@@ -144,8 +144,8 @@ function Timeline() {
       <div className="flex flex-col items-center gap-4">
       {grouped.map((timeline_items, i) => (
         <div key={`timeline-group-${i}`} className="flex gap-4">
-        {timeline_items.map((item, j) => (
-          <Button key={`timeline-item-${i * 69 + j}`} variant="reverse" className="bg-main h-auto" onClick={() => setCurrentIndex(0)}>
+        {timeline_items.map(({ item, index }) => (
+          <Button key={`timeline-item-${index}`} variant="reverse" className="bg-main h-auto" onClick={() => setCurrentIndex(index)}>
             <div className="flex flex-col w-[40vw] whitespace-normal gap-4 p-4">
               <div className="text-4xl font-[Nippo-Variable]">{item.title}</div>
               <div className="text-xl">{item.description}</div>
