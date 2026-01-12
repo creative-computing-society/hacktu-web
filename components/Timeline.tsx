5776c32c0a65a795f8db8e87e29b042c31595dc3
Aargh Rai <shubh2007rai@gmail.com>
2026-01-12 20:05:06 +0530

    auto click for those who don't understand

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
 import { TIMELINE } from "@/app/config"
 import { Calendar } from "@/components/ui/calendar";
 import { Button } from "@/components/ui/button";
@@ -12,13 +12,35 @@ export default function Timeline() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const switchToSingleColumn = useWidthCutoff(700);
   const verySmall = useWidthCutoff(484);
+  const HOVER_CLASS = "translate-x-boxShadowX translate-y-boxShadowY shadow-none"
+
+  const [buttonHoverState, setButtonHoverState] = useState(new Array(TIMELINE.length).fill(false) as boolean[])
+  const [intervalState, setIntervalState] = useState<any>(null);
+
+  useEffect(() => {
+    let i = 0;
+    let old_i = 0;
+    const interval = setInterval(() => {
+      const clone = buttonHoverState.slice();
+      clone[old_i] = false;
+      clone[i] = true;
+      setCurrentIndex(i);
+      setButtonHoverState(clone);
+      old_i = i;
+      i = (i + 1) % TIMELINE.length;
+    }, 1000);
+    setIntervalState(interval);
+
+    return () => clearInterval(interval)
+  }, [])
   return (
     <div id="timeline" className="flex flex-col items-center gap-4">
       <div className={cn("font-[Nippo-Variable] mb-16", verySmall ? "text-6xl" : "text-8xl")}>Timeline</div>
       <div className={cn("grid items-center gap-4", switchToSingleColumn ? "grid-cols-1" : "grid-cols-2")}>
       {TIMELINE.map((item, i) => (
-        <Button key={`timeline-item-${i}`} className={cn("bg-main h-full", SHADOWCOLOR[i % SHADOWCOLOR.length] + "shadow")} onClick={() => {
+        <Button key={`timeline-item-${i}`} className={cn("bg-main h-full", SHADOWCOLOR[i % SHADOWCOLOR.length] + "shadow", buttonHoverState[i] ? HOVER_CLASS : "")} onClick={() => {
           setCurrentIndex(i);
+          clearInterval(intervalState);
           const calendar = document.getElementById("timeline-calendar");
           if (calendar) calendar.scrollIntoView({
             behavior: 'smooth',
