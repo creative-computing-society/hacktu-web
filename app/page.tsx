c1f3f4cf8b7f0d4ca7c79919cb13583758f2c9f0
Aargh Rai <shubh2007rai@gmail.com>
2026-01-16 11:28:42 +0530

    trying to fit navan logo

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import ReachPatiala from "@/components/ReachPatiala";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
+    <div className="overflow-y-hidden flex flex-col items-center bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
       <Navbar items={NAVBAR} />
       <Hero />
       <div className="h-32"></div>
